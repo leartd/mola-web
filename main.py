68adae278706db38ef02e1ee5f64c946499d3846
leartd <leartdoko@gmail.com>
2015-02-07 13:34:34 -0500

    Initial Commit
    Basic Hello World base.

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import webapp2
+from google.appengine.ext.webapp import template
+
+
+def render_template(handler, templatename, templatevalues) :
+  path = os.path.join(os.path.dirname(__file__), 'templates/' + templatename)
+  html = template.render(path, templatevalues)
+  handler.response.out.write(html)
+
+  
+class MainPage(webapp2.RequestHandler) :
+  def post(self):
+    render_template(self, 'index.html', template_params)
+    
+  def get(self):
+    render_template(self, 'index.html', template_params)
+    
+
+app = webapp2.WSGIApplication([
+  ('/', MainPage)
+])
+
+
+
+
