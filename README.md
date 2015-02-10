ba11d041369e38d995dae3295f499d44af1d1832
Matballer <NolenJHeju@gmail.com>
2015-02-10 00:22:02 -0500

    awful basic changes
    end my life
    
    (I will have more done soon)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mola-web
 Mola is a reviews web app based on Google App Engine.
+
+Terminology:
+review - A user's submitted evaluation of a location, including at least one rating and review text
+rating - A score attributed to the quality of a particular attribute of a location
+	("accessibilty rating", "helpfulness rating")
+review text - A user's written evaluation of the restaurant's services and accommodation
+location - A venue of interest, such as a restaurant or store
+# fill this up with terms that need to be consistent in naming
+# also fix any you find off, and tell the group
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import webapp2
 from google.appengine.ext.webapp import template
 
 
-def render_template(templatename, templatevalues) :
+def render_template(templatename, templatevalues):
   path = os.path.join(os.path.dirname(__file__), 'templates/' + templatename)
   html = template.render(path, templatevalues)
   return html
 
   
-class MainPage(webapp2.RequestHandler) :
+class MainPage(webapp2.RequestHandler):
   def post(self):
     html = render_template('main_page.html', {})
     self.response.out.write(str(html))
@@ -17,6 +17,32 @@ class MainPage(webapp2.RequestHandler) :
   def get(self):
     html = render_template('main_page.html', {})
     self.response.out.write(str(html))
+
+    
+# class LocationPage():
+  # def post(self):
+  # def get(self):
+    
+
+class Review():
+  location_name = ''
+  location_address = ''
+  
+  vision_rating = 0.0
+  mobility_rating = 0.0
+  speech_rating = 0.0
+  helpfulness_rating = 0.0
+  
+  review_text = ''
+  
+  def __init__(ln, la, vr, mr, sr, hr, rt):
+    location_name = ln
+    location_address = la
+    vision_rating = vr
+    mobility_rating = mr
+    speech_rating = sr
+    helpfulness_rating = hr
+    review_text = rt
     
 
 app = webapp2.WSGIApplication([
