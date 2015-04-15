c5b719ff0c4e50370f9b9ddb0492e1d28d34b7d9
Matballer <JohnLeeSoftEng@gmail.com>
2015-04-15 12:47:03 -0400

    final deliverable or something
    probably yeah

--- static/scripts/maps.js
+++ static/scripts/maps.js
@@ -41,27 +41,27 @@ function locationMap() {
 //=============================================================================
 var map;
 function mainPageMap() {
-	// Get the user's coordinates.
-	if (latitude == null || longitude == null)
-	{
+	// Get the user's coordinates (nixed; unreliable)
+	// if (latitude == null || longitude == null)
+	// {
 		// navigator.geolocation is our fallback, because getCurrentPosition is highly unreliable
-		if (navigator.geolocation){
-			var geoOptions = {
-				maximumAge:15000,
-				timeout:10000
-			}
-			navigator.geolocation.getCurrentPosition(success, error, geoOptions);
-		}
+		// if (navigator.geolocation){
+			// var geoOptions = {
+				// maximumAge:15000,
+				// timeout:10000
+			// }
+			// navigator.geolocation.getCurrentPosition(success, error, geoOptions);
+		// }
 		
-		function success(position) {
-			latitude = position.coords.latitude; 
-			longitude = position.coords.longitude;
-			alert(latitude + ", " + longitude);
-		}
-		function error(err) {
-			alert("Error: " + err.code);
-		}
-	}
+		// function success(position) {
+			// latitude = position.coords.latitude; 
+			// longitude = position.coords.longitude;
+			// alert(latitude + ", " + longitude);
+		// }
+		// function error(err) {
+			// alert("Error: " + err.code);
+		// }
+	// }
 	
 	try {
 		var userCoords = new google.maps.LatLng(latitude, longitude);
@@ -89,7 +89,7 @@ function mainPageMap() {
 		zoomControl: true
 	}
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-		
+	
 	// Populate the main page map with markers and InfoWindows.
 	$.ajax({
 		type: "GET",
@@ -152,11 +152,21 @@ function createInfoWindow(marker, map, name, url, locTags){
 		content: infoContent
 	});
 	google.maps.event.addListener(marker, 'click', function() {
-		// map.setCenter(marker.position);
 		info.open(map, this);
 	});
 }
 
+// Refresh the map.
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+	refreshMap();
+});
+
+function refreshMap() {
+	google.maps.event.trigger(map, 'resize');
+	var latlng = new google.maps.LatLng(parseFloat(latitude), parseFloat(longitude));
+	map.setCenter(latlng);
+}
+	
 if((window.location.pathname).match("/location/.*")){
 	google.maps.event.addDomListener(window, 'load', locationMap);
 }
